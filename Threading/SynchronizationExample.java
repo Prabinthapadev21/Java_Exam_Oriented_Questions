d8aced083c5212991bb74a8556814853a2d0d379
Prabinthapadev21 <codinghub57@gmail.com>
2025-03-27 22:51:33 +0545

    Update SynchronizationExample.java

--- Threading/SynchronizationExample.java
+++ Threading/SynchronizationExample.java
@@ -1,23 +1,39 @@
 package Threading;
-class MultiplyTable
-{
-    public synchronized void table(int num)
-    {
-        for (int i = 1;i<=10;i++)
-        {
-            System.out.println(num+"*"+i+"="+num*i);
+class MultiplyTable {
+    public synchronized void table(int num) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num + "*" + i + "=" + num * i);
         }
     }
-    class A extends Thread
+}
+    class ThreadsA extends Thread
     {
+        MultiplyTable t;
+         public ThreadsA(MultiplyTable t)
+         {
+             this.t=t;
+         }
+
         public void run()
         {
-            
+            t.table(5);
         }
     }
-
-}
+    class ThreadsB extends Thread
+    {
+        MultiplyTable t;
+        public ThreadsB(MultiplyTable t)
+        {
+            this.t = t;
+        }
+        public void run()
+        {
+            t.table(10);
+        }
+    }
 public class SynchronizationExample {
     MultiplyTable mt = new MultiplyTable();
-
+    ThreadsA a = new ThreadsA(mt);
+    ThreadsB b = new ThreadsB(mt);
+    
 }
