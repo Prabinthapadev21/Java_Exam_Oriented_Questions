89e1fb54ccf79ce73859de0a0e00eb04c13127e4
Prabinthapadev21 <codinghub57@gmail.com>
2025-03-30 16:29:26 +0545

    ByteWriting in java

---
+++ FileHandeling/Byte-Oriented-Streaming/ByteWrite.java
@@ -0,0 +1,14 @@
+import java.io.*;
+public class ByteWrite {
+    public static void main(String[] args) {
+        try{
+            FileOutputStream fos = new FileOutputStream("/Users/prabinthapa/Desktop/ByteWrite.txt",true);
+            fos.write(65);
+            fos.flush();
+            fos.close();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
