3886f62331c5852709c337fbd625ede34d56af93
Prabinthapadev21 <codinghub57@gmail.com>
2025-03-30 21:05:41 +0545

    Updated

--- FileHandelingPraoject/NoteManager.java
+++ FileHandelingPraoject/NoteManager.java
@@ -14,6 +14,46 @@ public class NoteManager {
             System.out.println("4. Delete Note");
             System.out.println("5. Exit");
             System.out.print("Choose an option: ");
+            int choice = sc.nextInt();
+            sc.nextInt();
+            switch (choice)
+            {
+                case 1:
+                    createNote(scanner);
+                    break;
+                case 2:
+                    readNote();
+                    break;
+                case 3:
+                    appendNote(scanner);
+                    break;
+                case 4:
+                    deleteNote();
+                    break;
+                case 5:
+                    System.out.println("Exiting...");
+                    sc.close();
+                    return;
+                default:
+                    System.out.println("Invalid choice! Please try again.");
+            }
         }
     }
+    // create a new note
+    public static void createNote(Scanner scanner)
+    {
+        try(FileWriter writer = new FileWriter(File_Path))
+        {
+            System.out.println("Enter your note :");
+            String note = scanner.nextLine();
+            writer.write(note);
+            System.out.println("Note saved successfully");
+        }
+        catch (IOException e)
+        {
+            System.out.println("Error creating note.");
+            e.printStackTrace();
+        }
+    }
+
 }
