235d657fd59d15a98e6104f79a57ab8a1d90549a
Prabinthapadev21 <codinghub57@gmail.com>
2025-04-02 13:30:45 +0545

    Updated

---
+++ StringFunction/StringTrim.java
@@ -0,0 +1,10 @@
+package StringFunction;
+public class StringTrim {
+    public static void main(String[] args) {
+        String s = "  Prabin Thapa  ";
+        String a = s.trim();
+        System.out.println(a);
+        System.out.println(s);
+        //trim method() helps to remove the space before and after the string.
+    }
+}
