3de54950f266f11c373bdf588c3b212c1818e7ff
Prabinthapadev21 <codinghub57@gmail.com>
2025-04-02 13:15:12 +0545

    Updated

---
+++ StringFunction/StringConcenation.java
@@ -0,0 +1,11 @@
+package StringFunction;
+public class StringConcenation {
+    public static void main(String[] args) {
+        String s1 = "Prabin";
+        String s2 = "Thapa";
+        //We can concenate stirng using + operator and concat() method
+        String s3 = s1.concat(s2);
+        System.out.println(" "+s3);
+        System.out.println(s1+" "+s2);
+    }
+}
