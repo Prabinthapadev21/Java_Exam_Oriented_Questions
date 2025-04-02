419e4967a6368d33a0db41aae486d331c9bb2f8f
Prabinthapadev21 <codinghub57@gmail.com>
2025-04-02 13:25:40 +0545

    Updated

---
+++ StringFunction/StringReplace.java
@@ -0,0 +1,9 @@
+package StringFunction;
+public class StringReplace {
+    public static void main(String[] args) {
+        String s = "Prabin";
+        String b ="Thapa";
+        String a = s.replace(s,b);
+        System.out.println(a);
+    }
+}
