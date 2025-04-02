61992c3869ba5e0e2671d6822f825620679b0392
Prabinthapadev21 <codinghub57@gmail.com>
2025-04-02 08:26:49 +0545

    Character Extraction

---
+++ StringFunction/CharacterExtraction.java
@@ -0,0 +1,7 @@
+package StringFunction;
+public class CharacterExtraction {
+    public static void main(String[] args) {
+        String s = "Hello";
+        System.out.println(s.charAt(4));
+    }
+}
