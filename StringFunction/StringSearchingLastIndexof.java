01fef5e672ce464d45bd17e032dbd379dc141f7d
Prabinthapadev21 <codinghub57@gmail.com>
2025-04-02 13:02:53 +0545

    Updated Code

---
+++ StringFunction/StringSearchingLastIndexof.java
@@ -0,0 +1,9 @@
+package StringFunction;
+public class StringSearchingLastIndexof {
+    public static void main(String[] args) {
+        String s = "My Name is Prabin Thapa";
+        int index = s.lastIndexOf('a');
+        System.out.println(index);
+    }
+}
+// This method i used to return the last index of the character.
