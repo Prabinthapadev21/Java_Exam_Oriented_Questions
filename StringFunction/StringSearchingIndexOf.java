9009afe5494dea7ed66c93a391b6f7813b69518e
Prabinthapadev21 <codinghub57@gmail.com>
2025-04-02 12:59:27 +0545

    Updated

---
+++ StringFunction/StringSearchingIndexOf.java
@@ -0,0 +1,9 @@
+package StringFunction;
+public class StringSearchingIndexOf {
+    public static void main(String[] args) {
+        String s = "Prabin Thapa";
+        int index = s.indexOf('n');
+        System.out.println(index);
+    }
+}
+// This method is used to find out the index of the given character in the string.
