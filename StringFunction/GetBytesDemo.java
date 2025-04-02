e2a63b2f7d9bf74b05109d8546a88d8e9a77e9dc
Prabinthapadev21 <codinghub57@gmail.com>
2025-04-02 08:34:59 +0545

    Get Bytes from String

---
+++ StringFunction/GetBytesDemo.java
@@ -0,0 +1,8 @@
+package StringFunction;
+public class GetBytesDemo {
+    public static void main(String[] args) {
+        String s = "Prabin";
+        byte []b =s.getBytes();
+        System.out.println(b);
+    }
+}
