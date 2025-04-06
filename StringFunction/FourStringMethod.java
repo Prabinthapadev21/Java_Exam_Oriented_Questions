9ec72c4713112f00178002df531a86fca1a33670
Prabinthapadev21 <codinghub57@gmail.com>
2025-04-06 16:19:53 +0545

    updated

---
+++ StringFunction/FourStringMethod.java
@@ -0,0 +1,11 @@
+package StringFunction;
+
+public class FourStringMethod {
+    public static void main(String[] args) {
+        String s ="hello";
+        System.out.println(s.toUpperCase());
+        System.out.println(s.toLowerCase());
+        System.out.println(s.concat("prabin"));
+        System.out.println(s.charAt(1));
+    }
+}
