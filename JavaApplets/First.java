f766676db6b8bdbb31e362de1a39a5d707956001
Prabinthapadev21 <codinghub57@gmail.com>
2025-04-17 18:35:22 +0545

    Updated

---
+++ JavaApplets/First.java
@@ -0,0 +1,17 @@
+package JavaApplets;
+import java.applet.*;
+import java.awt.*;
+//public class First extends Applet{
+//    public void paint(Graphics g)
+//    {
+//        g.drawString("Welcome",150,150);
+//    }
+//}
+//First.html
+//<html>
+//<body>
+//<applet code ="First.class" width="300" height="300">
+//</applet>
+//</body>
+//</html>
+//this java applet is now not been used any more.
