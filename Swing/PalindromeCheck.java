f004e07f8a0c9df96bf013f8d6828b8f1414558b
Prabinthapadev21 <codinghub57@gmail.com>
2025-04-20 13:34:31 +0545

    Updated

---
+++ Swing/PalindromeCheck.java
@@ -0,0 +1,14 @@
+package Swing;
+
+import javax.swing.*;
+
+public class PalindromeCheck {
+    public void checker()
+    {
+        JFrame f = new JFrame("Palindrome Checker");
+        JLabel l = new JLabel("Enter a number");
+        l.setBounds(60,60,100,20);
+        JTextField t = new JTextField();
+
+    }
+}
--- Swing/PracticeDemo.java
+++ Swing/PracticeDemo.java
@@ -1,25 +1,47 @@
 package Swing;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 class PracticeDemo {
     public void Loginform() {
         JFrame f = new JFrame("Login Form");
-
+        f.setBackground(Color.RED);
+        f.getContentPane().setBackground(new Color(240, 248, 255));
         JLabel l1 = new JLabel("Username:");
+        l1.setForeground(Color.BLUE);
         l1.setBounds(60, 60, 80, 20);
         JTextField jf = new JTextField();
         jf.setBounds(160, 60, 100, 20);
 
         JLabel l2 = new JLabel("Password:");
+        jf.setForeground(Color.RED);
         l2.setBounds(60, 100, 80, 20);
         JPasswordField jp = new JPasswordField();
         jp.setBounds(160, 100, 100, 20);
 
+        JLabel l3 = new JLabel("Course Selection");
+        l3.setBounds(60,125,150,20);
+        JCheckBox c1 = new JCheckBox("Java");
+        c1.setBounds(160,160,100,20);
+        JCheckBox c2 = new JCheckBox("Python");
+        c2.setBounds(160,180,100,20);
+        JCheckBox c3 = new JCheckBox("PHP");
+        c3.setBounds(160,200,100,20);
+
+        JLabel l4 = new JLabel("Gender");
+        l4.setBounds(60,220,100,20);
+        JRadioButton jb1 = new JRadioButton("Male");
+        jb1.setBounds(60,240,100,20);
+        JRadioButton jb2 = new JRadioButton("Female");
+        jb2.setBounds(60,260,100,20);
+        JRadioButton jb3 = new JRadioButton("Others");
+        jb3.setBounds(60,280,100,20);
+
         JButton b = new JButton("Login");
-        b.setBounds(160, 140, 100, 30);
+        b.setBounds(160, 320, 100, 30);
 
         b.addActionListener(new ActionListener() {
             @Override
@@ -34,14 +56,24 @@ class PracticeDemo {
                 }
             }
         });
-
+        ButtonGroup bg = new ButtonGroup();
+        bg.add(jb1);
+        bg.add(jb2);
+        bg.add(jb3);
         f.add(l1);
         f.add(jf);
         f.add(l2);
         f.add(jp);
         f.add(b);
-
-        f.setSize(350, 300);
+        f.add(l3);
+        f.add(c1);
+        f.add(c2);
+        f.add(c3);
+        f.add(l4);
+        f.add(jb1);
+        f.add(jb2);
+        f.add(jb3);
+        f.setSize(1000, 1000);
         f.setLayout(null);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setVisible(true);
