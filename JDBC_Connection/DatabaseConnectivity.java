0efa0eca769bba032f8205fb920eebc095a25f3b
Prabinthapadev21 <codinghub57@gmail.com>
2025-04-20 21:08:46 +0545

    Updated code

--- JDBC_Connection/DatabaseConnectivity.java
+++ JDBC_Connection/DatabaseConnectivity.java
@@ -1,13 +1,22 @@
 //Java database connectivity
 /*
-1.import the package
-2.load and register the driver
-3.create connection
-4.create the statement
-5.Execute the query
+1.import the package --> java.sql
+2.load and register the driver -->com.mysql.jdbc.Driver
+3.create connection ---->Connection Interface
+4.create the statement ----> Statement Interface
+5.Execute the query ---->
 6.Process the result
 7.Close
  */
 package JDBC_Connection;
+import java.sql.*;
 public class DatabaseConnectivity {
+    public static void main(String[] args) throws Exception
+    {
+        String Url ="";
+        String username="";
+        String password="";
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection con = DriverManager.getConnection(Url,username,password);
+    }
 }
