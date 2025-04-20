5ae8ad45317249779ebaee10644aa442a6bf6fb0
Prabinthapadev21 <codinghub57@gmail.com>
2025-04-20 20:05:22 +0545

    Create DatabaseConnectivity.java

---
+++ JDBC_Connection/DatabaseConnectivity.java
@@ -0,0 +1,13 @@
+//Java database connectivity
+/*
+1.import the package
+2.load and register the driver
+3.create connection
+4.create the statement
+5.Execute the query
+6.Process the result
+7.Close
+ */
+package JDBC_Connection;
+public class DatabaseConnectivity {
+}
