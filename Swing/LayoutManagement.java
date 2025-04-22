12f164db4da05e6ff9aa5df729febc549a5a418e
Prabinthapadev21 <codinghub57@gmail.com>
2025-04-22 12:20:32 +0545

    Updated

---
+++ Swing/LayoutManagement.java
@@ -0,0 +1,28 @@
+package Swing;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class LayoutManagement {
+    public LayoutManagement()
+    {
+        JFrame f = new JFrame();
+        JButton b1 = new JButton("one");
+        JButton b2 = new JButton("two");
+        JButton b3 = new JButton("three");
+        JButton b4 = new JButton("four");
+        JButton b5 = new JButton("five");
+        f.add(b1, BorderLayout.NORTH);
+        f.add(b2, BorderLayout.SOUTH);
+        f.add(b3, BorderLayout.WEST);
+        f.add(b4, BorderLayout.EAST);
+        f.add(b5, BorderLayout.CENTER);
+        f.setSize(300,300);
+        f.setVisible(true);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        new LayoutManagement();
+    }
+}
