74315f76b812174f43629b5753d89ddf55c2a6d2
Prabin Thapa <codinghub57@gmail.com>
2025-04-26 21:07:44 +0545

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i;
+        for (i =0;i<3;i++)
+        {
+            String s1 = sc.nextLine();
+            int num = sc.nextInt();
+        }
+    }
+}
