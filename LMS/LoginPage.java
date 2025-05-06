dfa9eb29cd885daa117c144555c2805e812ebe62
Prabinthapadev21 <codinghub57@gmail.com>
2025-05-06 11:26:22 +0545

    Updated

---
+++ LMS/LoginPage.java
@@ -0,0 +1,35 @@
+package LMS;
+
+import javax.swing.*;
+
+public class LoginPage{
+
+    public LoginPage()
+    {
+        JFrame f = new JFrame("Login Page");
+
+
+        JLabel l1 = new JLabel("Sign up to LMS");
+        l1.setBounds(60,60,120,30);
+
+        JTextField t1 = new JTextField();
+        t1.setBounds(160,60,100,30);
+
+//            adding the components to the frame
+        f.add(l1);
+        f.add(t1);
+
+
+//            default operations starts
+        f.setSize(500,400);
+        f.setVisible(true);
+        f.setLayout(null);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//            default operations closes
+    }
+
+    public static void main(String[] args) {
+        new LoginPage();
+    }
+}
+
