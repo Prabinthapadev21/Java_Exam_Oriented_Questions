3d3fb1a8aaf76ba6c7eb0212e1118af69c0dede6
Prabinthapadev21 <codinghub57@gmail.com>
2025-05-06 11:30:18 +0545

    Update LoginPage.java

--- LMS/LoginPage.java
+++ LMS/LoginPage.java
@@ -12,7 +12,20 @@ public class LoginPage{
         JLabel l1 = new JLabel("Sign up to LMS");
         l1.setBounds(60,60,120,30);
 
-        
+        JLabel l2 = new JLabel("Username");
+        l2.setBounds(60,80,100,20);
+
+        JTextField t1 = new JTextField();
+        t1.setBounds(60,100,100,30);
+
+
+        JLabel l3 = new JLabel("Email");
+        l2.setBounds(60,120,100,20);
+
+        JTextField t2 = new JTextField();
+        t1.setBounds(60,140,100,30);
+
+
 //            adding the components to the frame
         f.add(l1);
 
