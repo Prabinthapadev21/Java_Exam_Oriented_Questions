93ed3d8ba9a52470a71dc38d5123704019f81062
Prabinthapadev21 <codinghub57@gmail.com>
2025-05-25 19:05:55 +0545

    updated

--- OOPsConcept/DemoExample.java
+++ OOPsConcept/DemoExample.java
@@ -1,23 +1,15 @@
 package OOPsConcept;
- abstract class HiDemo
-{
-    public void display()
-    {
-        System.out.println("This is a concrete method");
-    }
-    abstract void show();
-}
-class HelloDisplay extends HiDemo
-{
-    void show()
-    {
-        System.out.println("Hi this is a abstract method");
-    }
-}
-public class DemoExample {
-    public static void main(String[] args) {
-        HiDemo d = new HelloDisplay();
-        d.display();
-        d.show();
-    }
-}
+ class Single
+ {
+     public void display()
+     {
+         System.out.println("Hello");
+     }
+ }
+ class Double extends Single
+ {
+     public static void main(String[] args) {
+         Double d = new Double();
+         d.display();
+     }
+ }
