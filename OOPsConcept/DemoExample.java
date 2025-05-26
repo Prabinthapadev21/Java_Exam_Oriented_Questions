a5a3d8162c9c0aa332c4915893ec52cf3a6298b0
Prabinthapadev21 <codinghub57@gmail.com>
2025-05-26 07:19:00 +0545

    Updated

--- OOPsConcept/DemoExample.java
+++ OOPsConcept/DemoExample.java
@@ -1,18 +1,22 @@
 package OOPsConcept;
- class Single{
-     public void display()
-     {
-         System.out.println("Class Single");
-     }
- }
- class Double extends Single
+ interface Single
  {
-     public void show()
-     {
-         System.out.println("Class Double");
-     }
+     void display();
  }
- class Triple extends Double{
+ interface Double
+ {
+     void show();
+ }
+ class Triple implements Single,Double{
+    public void display()
+     {
+         System.out.println("This is interface 1");
+     }
+   public   void show()
+     {
+         System.out.println("This is interface 2");
+     }
+
      public static void main(String[] args) {
          Triple t = new Triple();
          t.display();
