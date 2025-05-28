d4bcda65e47e6f481bd7aaff68b017763768a726
Prabinthapadev21 <codinghub57@gmail.com>
2025-05-28 08:55:40 +0545

    Updated

--- OOPsConcept/DemoExample.java
+++ OOPsConcept/DemoExample.java
@@ -1,32 +1,22 @@
 package OOPsConcept;
- class Demo
- {
-     private String name;
-     private int age;
+// java doesnot supports multiple inheritence but by using the interface we can achieve multiple inheritence in java.
+class Demo
+{
+    public void display()
+    {
+        System.out.println("hi this is a parent Class");
+    }
+}
+class Demo2 extends Demo
+{
+    public void show()
+    {
+        System.out.println("This is a child class");
+    }
 
-     public void setName(String name)
-     {
-         this.name= name;
-     }
-     public String getName()
-     {
-         return name;
-     }
-
-     public void setAge(int age) {
-         this.age = age;
-     }
-
-     public int getAge() {
-         return age;
-     }
-
-     public static void main(String[] args) {
-         Demo d = new Demo();
-         d.setName("Prabin");
-         d.setAge(21);
-
-         System.out.println("Name = "+d.getName());
-         System.out.println("Age = "+d.getAge());
-     }
- }
+    public static void main(String[] args) {
+        Demo2 d = new Demo2();
+        d.show();
+        d.display();
+    }
+}
