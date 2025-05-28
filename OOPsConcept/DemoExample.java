7dc286618c0ef1587142ddca609000fa7160dfe4
Prabinthapadev21 <codinghub57@gmail.com>
2025-05-28 09:35:56 +0545

    Updated

--- OOPsConcept/DemoExample.java
+++ OOPsConcept/DemoExample.java
@@ -1,57 +1,70 @@
 package OOPsConcept;
 
-import java.util.Scanner;
-
-// java doesnot supports multiple inheritence but by using the interface we can achieve multiple inheritence in java.
-class Multiplication
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+class SwingDemo
 {
+    public SwingDemo()
+    {
+        JFrame f = new JFrame("Login Form");
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the size of row");
-        int m = sc.nextInt();
-        System.out.println("Enter the size of column");
-        int n = sc.nextInt();
-        int a[][] = new int[m][n];
-        int b[][] = new int[m][n];
-        int c[][] = new int[m][n];
-        System.out.println("Enter the element of frst matrix");
-        for (int i = 0;i<m;i++)
-        {
-            for (int j =0;j<n;j++)
-            {
-                a[i][j]= sc.nextInt();
-            }
-        }
-        System.out.println("Enter the element of 2nd matrix");
-        for (int i = 0;i<m;i++)
-        {
-            for (int j =0;j<n;j++)
-            {
-                b[i][j]= sc.nextInt();
-            }
-        }
+        JLabel l1 = new JLabel("Username");
+        l1.setBounds(60,60,80,20);
+        JTextField t1 = new JTextField();
+        t1.setBounds(160,60,100,30);
 
-//        logic for the multiplication of the matrix.
-        for (int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                for (int k =0;k<m;k++)
+
+        JLabel l2 = new JLabel("Password");
+        l2.setBounds(60,80,80,20);
+        JTextField t2 = new JTextField();
+        t2.setBounds(160,80,100,30);
+
+        JButton b = new JButton("Submit");
+        b.setBounds(160,100,80,30);
+
+
+//        adding the components to the frame
+        f.add(l1);
+        f.add(t1);
+        f.add(l2);
+        f.add(t2);
+        f.add(b);
+//        Default close operations
+        f.setSize(400,400);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setLayout(null);
+        f.setVisible(true);
+
+
+        b.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    String correctUsername = "Prabin";
+                    String correctPass = "prabin123";
+
+                    String username = t1.getText();
+                    String password = t2.getText();
+
+                    FileWriter fw = new FileWriter("exam.txt");
+
+                    if (correctUsername.equals(username) || correctPass.equals(password)) {
+                        fw.write(username);
+                        fw.write(password);
+                    }
+                }
+                catch (Exception ex)
                 {
-                    c[i][j]+= a[i][k]*b[k][j];
+                    ex.printStackTrace();
+                    JOptionPane.showMessageDialog(f,"Please accept the terms and condition first");
                 }
             }
-        }
+        });
+    }
 
-//        displaying the output
-        for (int i =0;i<m;i++)
-        {
-            for (int j =0;j<n;j++)
-            {
-                System.out.print(c[i][j]+" ");
-            }
-            System.out.println();
-        }
+    public static void main(String[] args) {
+        new SwingDemo();
     }
 }
