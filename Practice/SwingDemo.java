38e6e8199578c4b5950cd89fdd291be9e0c78ab6
Prabinthapadev21 <codinghub57@gmail.com>
2025-05-29 08:09:42 +0545

    updated

--- Practice/SwingDemo.java
+++ Practice/SwingDemo.java
@@ -1,44 +1,50 @@
 package Practice;
-import OOPsConcept.InterfaceDemo;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class SwingDemo {
-    public SwingDemo()
-    {
+    public SwingDemo() {
         JFrame f = new JFrame("Square Calculation");
 
-        JLabel l = new JLabel("Enter a number");
-        l.setBounds(60,60,100,30);
+        JLabel l1 = new JLabel("Enter a number:");
+        l1.setBounds(50, 50, 120, 30);
+
         JTextField t = new JTextField();
-        t.setBounds(160,60,80,30);
+        t.setBounds(180, 50, 100, 30);
 
         JButton b = new JButton("Calculate");
-        b.setBounds(160,140,100,30);
+        b.setBounds(140, 100, 100, 30);
+
+        JLabel l2 = new JLabel("Square of the number:");
+        l2.setBounds(30, 150, 140, 30);
 
         JTextField t1 = new JTextField();
-        t1.setBounds(160,160,100,30);
+        t1.setBounds(180, 150, 100, 30);
+        t1.setEditable(false); // optional: prevents editing result
 
-        f.add(l);
+        f.add(l1);
         f.add(t);
         f.add(b);
+        f.add(l2);
         f.add(t1);
 
-//        default operations
-        f.setSize(400,400);
-        f.setVisible(true);
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setSize(400, 300);
         f.setLayout(null);
-
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setVisible(true);
 
         b.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                int num = Integer.parseInt(t.getText());
-                int square = num*num;
-                t1.setText("The square of number is "+square);
+                try {
+                    int num = Integer.parseInt(t.getText());
+                    int square = num * num;
+                    t1.setText(String.valueOf(square));
+                } catch (NumberFormatException ex) {
+                    JOptionPane.showMessageDialog(f, "Please enter a valid number.");
+                }
             }
         });
     }
