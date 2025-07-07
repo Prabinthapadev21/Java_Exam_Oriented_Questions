9d7aad11dc22de7923f3290ce936f4d80f7745d1
Prabinthapadev21 <codinghub57@gmail.com>
2025-07-07 08:49:54 +0545

    Create QuestionTwo.java

---
+++ FinalExaminationPractice/QuestionTwo.java
@@ -0,0 +1,10 @@
+package FinalExaminationPractice;
+//What is inheritence. What are the types of the inheritence.
+//Inheritence is a oops concept in which the child class(subclass) can inherit the properties of the parent class(superclass).
+//The types of  inheritence are:
+//1.Single Inheritence
+//2.Multiple Inheritence
+//3.Multi-Level Inheritence
+//4.Heircharcial Inheritence
+public class QuestionTwo {
+}
