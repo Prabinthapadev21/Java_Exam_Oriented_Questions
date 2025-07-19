b197a80d0bff2183eb8c680437baf5269757c8f7
Prabinthapadev21 <codinghub57@gmail.com>
2025-07-19 11:11:56 +0545

    Create TwentyOneQuestion.java

---
+++ Final_Examination/set_1/TwentyOneQuestion.java
@@ -0,0 +1,22 @@
+package Final_Examination.set_1;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+public class TwentyOneQuestion {
+    public TwentyOneQuestion()
+    {
+        JFrame f = new JFrame("Add and Subtract");
+
+        JLabel l1 = new JLabel("NumberOne:");
+        l1.setBounds(60,);
+
+        JLabel l2 = new JLabel("NumberTwo");
+        l2.setBounds();
+
+//        default operations
+        f.setSize(400,500);
+        f.setLayout(null);
+        f.setVisible(true);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
