3da380d8581073cc79448435f7b96cc1882a470d
Prabinthapadev21 <codinghub57@gmail.com>
2025-07-19 08:11:14 +0545

    updated code

--- Final_Examination/set_1/TwentyFourQuestion.java
+++ Final_Examination/set_1/TwentyFourQuestion.java
@@ -27,11 +27,30 @@ public class TwentyFourQuestion {
         JPasswordField f3 = new JPasswordField();
         f3.setBounds(160,140,100,30);
 
+        JLabel l4 = new JLabel("Gender");
+        l4.setBounds(60,180,80,20);
+
+        JRadioButton r1 = new JRadioButton("Male");
+        r1.setBounds(160,180,100,30);
+        JRadioButton r2 = new JRadioButton("Female");
+        r2.setBounds(230,180,100,30);
+        JLabel l5 = new JLabel("DOB");
+        l5.setBounds(60,220,80,20);
+        String day[] = {"1","2","3","4","5"};
+        String month[] = {"Jan","Feb","Mar","Apr","May"};
+        String year[] = {"2006","2005","2004","2003"};
+        JComboBox box = new JComboBox(day);
+        JComboBox box1 = new JComboBox(month);
+        JComboBox box2 = new JComboBox(year);
+        box.setBounds(160,220,60,20);
+        box1.setBounds(200,220,75,20);
+        box2.setBounds(260,220,90,20);
+
         JCheckBox check = new JCheckBox("Terms and Condition");
-        check.setBounds(60,200,200,20);
+        check.setBounds(60,260,200,20);
 
         JButton jb = new JButton("Submit");
-        jb.setBounds(160,250,100,30);
+        jb.setBounds(160,300,100,30);
 
 //        Default operations
         f.setSize(400,500);
@@ -41,6 +60,13 @@ public class TwentyFourQuestion {
         f.add(f1);
         f.add(f2);
         f.add(f3);
+        f.add(l4);
+        f.add(r1);
+        f.add(r2);
+        f.add(l5);
+        f.add(box);
+        f.add(box1);
+        f.add(box2);
         f.add(check);
         f.add(jb);
 
@@ -55,12 +81,25 @@ public class TwentyFourQuestion {
                 String name = f1.getText();
                 String email = f2.getText();
                 String password = new String(f3.getPassword());
+                String gender ="";
+                String dob ="";
+
+                if(r1.isSelected())
+                {
+                     gender = r1.getText();
+                }
+                else {
+                     gender = r2.getText();
+                }
+                dob = box.getSelectedItem()+ " "+box1.getSelectedItem()+ " "+box2.getSelectedItem();
 
                 if (check.isSelected()) {
                     try (FileWriter fw = new FileWriter("exam.txt")) {
                         fw.write("Name = " + name + "\n");
                         fw.write("Email = " + email + "\n");
                         fw.write("Password = " + password + "\n");
+                        fw.write("Gender = "+gender+"\n");
+                        fw.write("Dob = "+dob + "\n" );
                         JOptionPane.showMessageDialog(f, "Data saved successfully!");
                     } catch (IOException ex) {
                         JOptionPane.showMessageDialog(f, "Error: " + ex.getMessage());
